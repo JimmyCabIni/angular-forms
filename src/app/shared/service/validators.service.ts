7164c8d47255453293a10b93e70b91b4506aa3e4
Jaime <jaimecaballero99@gmail.com>
2024-04-17 16:02:22 +0200

    Validadores asíncronos

--- src/app/shared/service/validators.service.ts
+++ src/app/shared/service/validators.service.ts
@@ -7,7 +7,7 @@ export class ValidatorsService {
   public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
-
+  // VALIDADOR ASÍNCRONO
   // Comprueba si el valor q recibe === strider
   // Si se cumple devuelve el mensaje de error
   public cantBeStrider = (control: FormControl): ValidationErrors | null => {
